bd20b470734028d2ba63b7ae86624ec435cf6880
Aron <1041111561@qq.com>
2023-08-07 16:13:13 +0800

    wrap functions.

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,15 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  // Convert absolute seqno -> seqno
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  // Convert seqno -> absolute seqno
+  uint8_t move_bits;
+  for ( move_bits = 0; checkpoint > INT32_MAX; ++move_bits )
+    checkpoint >>= 1;
+  return static_cast<uint64_t>( zero_point.raw_value_ ) << move_bits;
 }
