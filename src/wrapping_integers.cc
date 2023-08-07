bda3a7729d9fea51c8daea8818e46b4b303e788b
Aron <1041111561@qq.com>
2023-08-07 17:24:43 +0800

    wrap32 functions.

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -2,17 +2,23 @@
 
 using namespace std;
 
+/* Convert absolute seqno -> seqno */
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Convert absolute seqno -> seqno
-  return zero_point + static_cast<uint32_t>( n );
+  return zero_point + n;  // N convert from uint64_t to uint32_t implicitly.
 }
 
+/* Convert seqno -> absolute seqno */
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Convert seqno -> absolute seqno
-  uint8_t move_bits;
-  for ( move_bits = 0; checkpoint > INT32_MAX; ++move_bits )
-    checkpoint >>= 1;
-  return static_cast<uint64_t>( zero_point.raw_value_ ) << move_bits;
+  uint64_t add_num = 1 << 32;
+  uint64_t zero_point_64 = zero_point.raw_value_;
+  bool move = false;
+  while ( zero_point_64 < checkpoint ) {
+    zero_point_64 += add_num;
+    move = true;
+  }
+  if (move && zero_point_64 - checkpoint > checkpoint - (zero_point_64 - add_num))
+    zero_point_64 -= add_num;
+  return zero_point_64;
 }
