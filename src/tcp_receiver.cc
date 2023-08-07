8d49de6e38d2df3ecd86aa9b07159629506337b3
Aron <1041111561@qq.com>
2023-08-07 22:23:52 +0800

    check2 sth.

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -2,19 +2,38 @@
 
 using namespace std;
 
-void TCPReceiver::receive( TCPSenderMessage &message, Reassembler& reassembler, Writer& inbound_stream )
+void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream )
 {
- // 1. Set the Initial Sequence Number if necessary.
- // 2. Push any data to the Reassembler.
-
-
-
-
+  if ( !isn && !message.SYN )
+    // 未建立TCP连接时直接丢弃非SYN包
+    return;
+  if ( !isn && message.SYN )
+    // 建立TCP连接
+    isn = message.seqno;
+  const uint64_t check_point = inbound_stream.bytes_pushed() + 1;              // stream index to abs_seqnno
+  const uint64_t abs_seqno = message.seqno.unwrap( isn.value(), check_point ); // abs_seqno
+  // convert to stream index
+  const uint64_t first_index = message.SYN ? 0 : abs_seqno - 1;
+  reassembler.insert( first_index, std::move( message.payload ), message.FIN, inbound_stream );
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
-  // Your code here.
-  (void)inbound_stream;
-  return {};
+  TCPReceiverMessage send_msg;  
+  if ( !isn ) {
+  // 未建立TCP连接
+    send_msg.ackno = nullopt;
+    send_msg.window_size = u64ToU16(inbound_stream.available_capacity() + 2); // + SYN + FIN
+  } else {
+  // 已建立TCP连接
+    const uint64_t abs_seqno = inbound_stream.bytes_pushed() + 1 + inbound_stream.is_closed(); // stream index to abs_seq index
+    send_msg.ackno = Wrap32::wrap(abs_seqno, isn.value());
+    send_msg.window_size = u64ToU16(inbound_stream.available_capacity() +  inbound_stream.is_closed() ); // + FIN?
+  }
+  return send_msg;
+}
+
+inline uint16_t TCPReceiver::u64ToU16(uint64_t num_64) const
+{
+  return num_64 > UINT16_MAX ? UINT16_MAX : num_64;  
 }
