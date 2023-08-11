eafdf48499970087a8e0976fe6d12d50d028e0d0
Aron <1041111561@qq.com>
2023-08-11 20:42:14 +0800

    temporary save.

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -5,9 +5,17 @@
 
 using namespace std;
 
+inline void Timer::reset()
+{
+  round_time_ = 0;
+  uint64_t RTO_ms_ = 0;
+  is_running_ = false;
+  is_expired_ = false;
+}
+
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
+  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms ), retrans_timer_()
 {}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
@@ -48,6 +56,24 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  if ( retrans_timer_.is_running() && !retrans_timer_.is_expired() ) {
+    retrans_timer_.increase_round_time( ms_since_last_tick );
+    if ( retrans_timer_.get_round_time() >= cur_RTO_ms_ )
+      retrans_timer_.set_expired();
+  }
+
+  if ( !retrans_timer_.is_expired() )
+    return;
+  
+  // 1. 重传最早的TCP段
+  
+  // 2. 窗口尺寸非0
+  //    1. 记录连续重传次数
+  //    2. RTO加倍
+  
+  
+  
+  // 3. 重置重传定时器并启动它
+  retrans_timer_.reset();
+  retrans_timer_.start(cur_RTO_ms_);
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,10 +4,40 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+#include <deque>
+
+class Timer
+{
+private:
+  size_t round_time_ = 0;   // 未判定为超时的累积计时
+  uint64_t RTO_ms_ = 0;
+  bool is_running_ = false; // 运行状态
+  bool is_expired_ = false; // 过期状态
+public:
+  Timer() = default;
+  Timer( const Timer& other ) = delete;
+  Timer& operator=( const Timer& other ) = delete;
+  // ~Timer() {}
+
+  inline void start(const uint64_t cur_RTO_ms) { is_running_ = true; RTO_ms_ = cur_RTO_ms; }
+  inline void stop() { is_running_ = false; }
+  inline bool is_running() const { return is_running_; }
+  inline bool is_expired() const { return is_expired_; }
+  inline size_t get_round_time() const { return round_time_; }
+  inline void increase_round_time(const size_t ms) { round_time_ += ms; }
+  inline void set_expired() { is_expired_ = true; }
+  inline void reset();
+};
+
 class TCPSender
 {
+private:
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  uint64_t cur_RTO_ms_;
+  
+  Timer retrans_timer_;
+  std::deque<>
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
