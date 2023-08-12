73ff14deee370a16a6494fb7904b685fc08f715b
Aron <1041111561@qq.com>
2023-08-12 16:21:41 +0800

    check3 version1

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,57 +1,103 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
 
+#include <optional>
 #include <random>
-
 using namespace std;
-
-inline void Timer::reset()
-{
-  round_time_ = 0;
-  uint64_t RTO_ms_ = 0;
-  is_running_ = false;
-  is_expired_ = false;
-}
 
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms ), retrans_timer_()
+  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) )
+  , initial_RTO_ms_( initial_RTO_ms )
+  , cur_RTO_ms_( initial_RTO_ms )
+  , next_abs_seqno_( 0 )
+  , last_ackno_( isn_ )
+  , windows_size_( 1 )
+  , consecutive_retrans_cnt_( 0 )
+
+  , retrans_timer_()
+  , send_segments_()
+  , track_segments_()
 {}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+  const uint64_t last_abs_ackno = last_ackno_.unwrap( isn_, next_abs_seqno_ );
+  return next_abs_seqno_ - last_abs_ackno;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return consecutive_retrans_cnt_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-  // Your code here.
-  return {};
+  optional<TCPSenderMessage> seg_maybe_send;
+  // 计时器过期，重传最早的TCP段（若存在)
+  if ( retrans_timer_.is_expired() && !track_segments_.empty() ) {
+    seg_maybe_send = track_segments_.front();
+    track_segments_.pop();
+    ++consecutive_retrans_cnt_; // 记录连续重传次数, 没有连续重传的时候要置为0
+  }
+  // 发送缓存中的TCP段
+  else if ( !send_segments_.empty() ) {
+    seg_maybe_send = send_segments_.front();
+    send_segments_.pop();
+  }
+  return seg_maybe_send;
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+  // 从 outbound_stream 读取相应字节数，并封装成 TCP segment放入发送队列
+  const uint64_t payload_size = min( TCPConfig::MAX_PAYLOAD_SIZE, outbound_stream.bytes_buffered() );
+  string payload;
+  while ( payload.size() != payload_size ) {
+    string_view next = outbound_stream.peek();
+    uint64_t bytes_to_pop = next.size();
+    if ( payload.size() + bytes_to_pop > payload_size )
+      bytes_to_pop = payload_size - ( payload.size() + bytes_to_pop );
+    payload += next.substr( 0, bytes_to_pop );
+    outbound_stream.pop( bytes_to_pop );
+  }
+  TCPSenderMessage seg_to_send;
+  // 此处的payload_size = seq数
+  seg_to_send.seqno = Wrap32::wrap( outbound_stream.bytes_popped() - payload_size, isn_ );
+  seg_to_send.SYN = false;
+  seg_to_send.payload = payload;  
+  seg_to_send.FIN = false;
+  send_segments_.push(std::move(seg_to_send));
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
-  // Your code here.
-  return {};
+  // 不占据序列号，也不追踪和重发
+  Wrap32 seqno = Wrap32::wrap( next_abs_seqno_, isn_ );
+  return { seqno, false, {}, false };
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  windows_size_ = msg.window_size;
+
+  if ( msg.ackno ) {
+    cur_RTO_ms_ = initial_RTO_ms_;       // 重置RTO
+    consecutive_retrans_cnt_ = 0;        // 重置连续重传计数器
+    while ( !track_segments_.empty() ) { // 移除buffer中已经被确认的segments
+      const auto& front = track_segments_.front();
+      const uint64_t front_abs_seqno = front.seqno.unwrap( isn_, next_abs_seqno_ );
+      const uint64_t lower_bound = msg.ackno.value().unwrap( isn_, next_abs_seqno_ );
+      if ( front_abs_seqno < lower_bound )
+        track_segments_.pop();
+      else
+        break;
+    }
+    if ( track_segments_.empty() )
+      retrans_timer_.stop(); // 所有segment都被接收， 停止timer
+    else
+      retrans_timer_.restart( cur_RTO_ms_ ); // 重启定时器
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
@@ -62,18 +108,15 @@ void TCPSender::tick( const size_t ms_since_last_tick )
       retrans_timer_.set_expired();
   }
 
-  if ( !retrans_timer_.is_expired() )
-    return;
-  
-  // 1. 重传最早的TCP段
-  
-  // 2. 窗口尺寸非0
-  //    1. 记录连续重传次数
-  //    2. RTO加倍
-  
-  
-  
-  // 3. 重置重传定时器并启动它
-  retrans_timer_.reset();
-  retrans_timer_.start(cur_RTO_ms_);
+  // 3. 重启重传定时器
+  retrans_timer_.restart( cur_RTO_ms_ );
+}
+
+/* Timer function definations */
+inline void Timer::reset()
+{
+  round_time_ = 0;
+  uint64_t RTO_ms_ = 0;
+  is_running_ = false;
+  is_expired_ = false;
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,7 +4,8 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
-#include <deque>
+#include <queue>
+#include <optional>
 
 class Timer
 {
@@ -14,19 +15,22 @@ private:
   bool is_running_ = false; // 运行状态
   bool is_expired_ = false; // 过期状态
 public:
-  Timer() = default;
-  Timer( const Timer& other ) = delete;
-  Timer& operator=( const Timer& other ) = delete;
+  // Timer() = default;
+  // Timer( const Timer& other ) = delete;
+  // Timer& operator=( const Timer& other ) = delete;
   // ~Timer() {}
 
   inline void start(const uint64_t cur_RTO_ms) { is_running_ = true; RTO_ms_ = cur_RTO_ms; }
   inline void stop() { is_running_ = false; }
+  inline void reset();
+  inline void restart(const uint64_t cur_RTO_ms) { reset(); start(cur_RTO_ms);}
+  
+  inline void increase_round_time(const size_t ms) { round_time_ += ms; }
+  inline void set_expired() { is_expired_ = true; }
+  
   inline bool is_running() const { return is_running_; }
   inline bool is_expired() const { return is_expired_; }
   inline size_t get_round_time() const { return round_time_; }
-  inline void increase_round_time(const size_t ms) { round_time_ += ms; }
-  inline void set_expired() { is_expired_ = true; }
-  inline void reset();
 };
 
 class TCPSender
@@ -35,9 +39,14 @@ private:
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
   uint64_t cur_RTO_ms_;
-  
+  uint64_t next_abs_seqno_;  // 发送的/下一个序列号数
+  Wrap32 last_ackno_;  // 最后接收到的ack序号
+  uint16_t windows_size_;
+  uint64_t consecutive_retrans_cnt_;
+
   Timer retrans_timer_;
-  std::deque<>
+  std::queue<TCPSenderMessage> send_segments_;  // 要发送的TCP段队列
+  std::queue<TCPSenderMessage> track_segments_;  // 追踪已经发出但未被确认的tcp段
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
